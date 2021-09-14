9e0fc75887289ce16f55d0a386e8e53b2de46065
thisispawann <pawanstha.6964@gmail.com>
2021-09-14 09:24:41 +0545

    blog page added

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Blog
+from .views import Blog, FrontEnd
 
 urlpatterns = [
     path('', Blog, name='blog'),
+    path('frontendcodes/', FrontEnd, name='frontend'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def Blog(request):
     return render(request, 'blog.html')
+
+#Front end assets 
+def FrontEnd(request):
+    return render(request, 'frontend.html')
